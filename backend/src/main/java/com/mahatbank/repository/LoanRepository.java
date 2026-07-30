448f79788d4466c8a5f7d0325964ddb83321403f
pm725 <mahatpriyanshu7@gmail.com>
2026-07-30 14:16:15 +0545

    Adding repository for Loan

---
+++ backend/src/main/java/com/mahatbank/repository/LoanRepository.java
@@ -0,0 +1,12 @@
+package com.mahatbank.repository;
+
+import com.mahatbank.model.Loan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LoanRepository extends JpaRepository<Loan, Long> {
+    List<Loan> findByUserId(Long userId);
+}
